e7c491fd006ba1815e3fd90f2f24785d1423e31e
GT610 <myddz1005@163.com>
2025-03-26 11:47:59 +0800

    Get properties through AGI

--- src/web/File Manager/functions/getProperties.js
+++ src/web/File Manager/functions/getProperties.js
@@ -2,4 +2,36 @@ if (!requirelib("filelib")) {
     console.log("Filelib import failed");
     sendResp("Filelib import failed");
 }
-console.log("Success getProperties")
+
+// Parse basic info
+var basename = path.split('/').pop();
+var parts = path.split('/');
+parts.pop();
+var virtualDirname = parts.join('/');
+
+// Parse Ext
+var ext = '.' + (basename.split('.').pop() || '');
+
+// Get info with filelib
+var filesize = filelib.filesize(path);
+var isDirectory = filelib.isDir(path);
+var lastModTime = filelib.mtime(path, false);
+var lastModUnix = filelib.mtime(path, true);
+
+// Result object
+var result = {
+    VirtualPath: path,
+    Basename: basename,
+    VirtualDirname: virtualDirname,
+    Ext: ext,
+    MimeType: "MimeType unsupported",
+    Filesize: filesize,
+    Permission: "Permission unsupported",
+    LastModTime: lastModTime,
+    LastModUnix: lastModUnix,
+    IsDirectory: isDirectory,
+    Owner: "Owner unsupported"
+};
+
+// Return JSON
+sendJSONResp(result);
